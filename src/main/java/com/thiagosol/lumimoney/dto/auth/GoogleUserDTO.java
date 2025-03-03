1a77174c81088189feb954ea87816c7e7dee8f7f
Thiago Sol <thiagosol@ymail.com>
2025-03-03 01:14:07 -0300

    Update GoogleUserDTO.java

--- src/main/java/com/thiagosol/lumimoney/dto/auth/GoogleUserDTO.java
+++ src/main/java/com/thiagosol/lumimoney/dto/auth/GoogleUserDTO.java
@@ -1,4 +1,7 @@
 package com.thiagosol.lumimoney.dto.auth;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public record GoogleUserDTO(String email, String name, String picture) {
 }
